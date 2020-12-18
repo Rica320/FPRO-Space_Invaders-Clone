374a96ab0a1643963c4dfa8e8f6637ca3f94d7b5
Rica320 <68922924+Rica320@users.noreply.github.com>
2020-12-18 18:35:18 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 17:23:09 2020
+
+@author: ricardo
+"""
+
+import pygame
+import time 
+import random 
+
+win = pygame.display.set_mode((1000,1000))
+
+# images 
+
+ship = [pygame.image.load('Images/ship.png')]
+
+#sounds
+
+pygame.mixer.init()
+pygame.mixer.music.load('sounds/spaceinvaders1.ogg')
+pygame.mixer.music.play()
+
+pygame.init()
+
+class Ship(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        self.pos = pos 
+        # self.music = 
+
+def menu():
+    pass
+
+def main():
+    run = True 
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+             pygame.mixer.music.fadeout(1000)
+    pygame.quit()
+
+main()
